86bd90ccbf099c13359dd95b1fa0f16bbdb0ad4a
Haley <hne3@github.com>
2015-09-03 09:34:48 -0400

    Added gun, firing bullets.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+    private GameObject bullet;
+    private Rigidbody bulletRigidBody;
+
+    private Vector3 offset = new Vector3(0.0f, 0.0f, 1.0f);
+    private Vector3 firingForce = new Vector3(0.0f, 0.0f, 1000.0f);
+
+    private WaitForSeconds bulletTimeout = new WaitForSeconds(5.0f);
+    private bool firing = false;
+        
+    void Start()
+    {
+    }
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+            StartCoroutine(TimeOut());
+        }
+	}
+	
+	private void Fire () {
+        if(bullet == null)
+        {
+            bullet = Instantiate(bulletPrefab) as GameObject;
+            bulletRigidBody = bullet.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            bulletRigidBody.isKinematic = true;
+            bulletRigidBody.isKinematic = false;
+        }
+        bullet.transform.position = transform.position + offset;
+        bulletRigidBody.AddForce(firingForce);
+    }
+
+    private IEnumerator TimeOut()
+    {
+        yield return bulletTimeout;
+        if (bullet != null)
+        {
+            Destroy(bullet);
+        }
+
+    }
+    
+}
