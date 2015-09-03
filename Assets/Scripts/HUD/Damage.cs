bb46013afba80bd8e50b83f8664750ccc4730547
Benjamin Ratner <bmr57@case.edu>
2015-09-03 15:27:37 -0400

    add heath bar slider, regen functionality

---
+++ Assets/Scripts/HUD/Damage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+	PlayerHealth playerHealth;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		playerHealth = player.GetComponent <PlayerHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F)){
+			playerHealth.TakeDamage(25);
+		}
+	}
+}
