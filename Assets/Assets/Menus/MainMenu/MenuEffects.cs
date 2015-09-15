d45eed6efa6445426fae85576f47b5a0592409f6
Christian Gunderman <gundermanc@gmail.com>
2015-09-15 08:00:06 -0400

    MenuEffects component for perspective and rotate.

---
+++ Assets/Assets/Menus/MainMenu/MenuEffects.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Effects for making menus and other game objects rotate continuously at a constant
+/// rate and wiggle in perspective as the mouse is moved.
+/// </summary>
+public class MenuEffects : MonoBehaviour
+{
+    /// <summary>
+    /// Enables perspective wiggling.
+    /// </summary>
+    public bool enableWiggle;
+
+    /// <summary>
+    /// Enables continous rotate.
+    /// </summary>
+    public bool enableRotate;
+
+    /// <summary>
+    /// Vector for continous rotation direction.
+    /// </summary>
+    public Vector3 rotationVector;
+
+    /// <summary>
+    /// Rate at which rotation occurs.
+    /// </summary>
+    public float degreesPerSecond;
+
+    /// <summary>
+    /// Max travel for perspective wiggle.
+    /// </summary>
+    public float maxWiggleDegrees = 25;
+
+    /// <summary>
+    /// Stores the original orientation for the perspective wiggle.
+    /// </summary>
+    private Vector3 originalRotation;
+
+    /// <summary>
+    /// Initialization.
+    /// </summary>
+    void Start()
+    {
+        this.originalRotation = gameObject.transform.rotation.eulerAngles;
+    }
+
+    /// <summary>
+    /// Frame update.
+    /// </summary>
+    void Update()
+    {
+        // Rotate object to original orientation.
+        transform.rotation = Quaternion.FromToRotation(transform.rotation.eulerAngles, originalRotation);
+
+        // Float from -1.0f to 1.0f of mouse percentage distances from center.
+        float xFromCenter = -((Input.mousePosition.x - (Screen.width / 2)) / (Screen.width / 2));
+        float yFromCenter = (Input.mousePosition.y - (Screen.height / 2)) / (Screen.height / 2);
+
+        // Rotation vectors:
+        var rotation = this.enableRotate ? rotationVector * Time.timeSinceLevelLoad 
+            * degreesPerSecond : Vector3.zero;
+        var wiggle = this.enableWiggle ? new Vector3(maxWiggleDegrees * yFromCenter, maxWiggleDegrees
+            * xFromCenter, 0) : Vector3.zero;
+
+        gameObject.transform.Rotate(rotation + wiggle);
+    }
+}
