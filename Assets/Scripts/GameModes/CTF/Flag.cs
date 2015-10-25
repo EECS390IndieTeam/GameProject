7e15413bef3a80cba5cc4ab7a8ecfb9f5b680cee
James Wright <12wrigja@gmail.com>
2015-10-25 16:48:31 -0400

    Fixed pickup delay on the flag.

--- Assets/Scripts/GameModes/CTF/Flag.cs
+++ Assets/Scripts/GameModes/CTF/Flag.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class Flag : MonoBehaviour {
 
     public int teamID; //The team that this flag belongs to
-    public IPlayer player; //The player holding this flag. Null if the flag is dropped.
+    public AbstractPlayer player; //The player holding this flag. Null if the flag is dropped.
     private Vector3 flagSpawnPosition;
 	private Quaternion flagSpawnRotation;
+	private float timeDelay = 3.0f;
+	private bool isEnabled = true;
     Collider c;
 
 	// Use this for initialization
@@ -21,19 +23,28 @@ public class Flag : MonoBehaviour {
         DropFlag();
         this.transform.position = flagSpawnPosition;
         this.transform.rotation = flagSpawnRotation;
-        c.isTrigger = true;
     }
 
-    public void DropFlag()
+	public void DropFlag(){
+		StartCoroutine ("DropFlagRoutine");
+	}
+
+    IEnumerator DropFlagRoutine()
     {
-        player = null;
+		transform.position += 2 * player.transform.forward;
+		player = null;
         this.transform.parent = null;
-        c.isTrigger = true;
+		yield return new WaitForSeconds(timeDelay);
+		Debug.Log ("Re-enabling flag");
+        isEnabled = true;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        IPlayer p = other.gameObject.GetComponentInParent<IPlayer>();
+		if (!isEnabled) {
+			return;
+		}
+        AbstractPlayer p = other.gameObject.GetComponentInParent<AbstractPlayer>();
         CaptureTheFlagMode mode = (CaptureTheFlagMode)GameManager.instance.gameMode;
         //You can only pick up this flag if
         // - you are a player
@@ -46,7 +57,15 @@ public class Flag : MonoBehaviour {
             //Update who is holding flag
             this.gameObject.transform.parent = other.gameObject.transform;
             player = p;
-            c.isTrigger = false;
+            isEnabled = false;
         }
     }
+
+	void Update(){
+		if (player != null) {
+			if(Input.GetButtonDown("Fire1")){
+				DropFlag();
+			}
+		}
+	}
 }
