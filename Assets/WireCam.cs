01af41a21f158b50d187b0b10135bcdcb98d3a9e
a-hacker <explosivepyrounicorns@gmail.com>
2015-10-25 14:23:45 -0400

    Adjusted camera size and position. Optimized rendering.
    Currently, the script the WireCam script dynamically changes the quality
    level for the minimap. I don't know if this is truly more efficent but my
    FPS jumped from ~160 to ~180 with this change. More testing on another
    computer to see if this is sufficient is neccesary.

--- Assets/WireCam.cs
+++ Assets/WireCam.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class WireCam : MonoBehaviour {
 
+	int normal_quality;
+
 	// Use this for initialization
 	void Start () {
-	
+		normal_quality = QualitySettings.GetQualityLevel();
 	}
+
 
 	void OnPreRender(){
 		GL.wireframe = true;
-		GL.Color (Color.blue);
-
+		QualitySettings.SetQualityLevel (1, false);
 	}
 
 	void OnPostRender(){
 		GL.wireframe=false;
+		QualitySettings.SetQualityLevel (normal_quality, false);
 	}
 
 	// Update is called once per frame
