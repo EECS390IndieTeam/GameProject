38ac5607257bcf88849458ca6b58a816f13c4291
Steven Miller <yourgamesbeover@gmail.com>
2015-10-25 17:26:51 -0400

    scene loading for CTF

--- Assets/Scripts/Networking/Debug/DebugLobbyMenu.cs
+++ Assets/Scripts/Networking/Debug/DebugLobbyMenu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class DebugLobbyMenu : Bolt.GlobalEventListener {
 
@@ -7,18 +8,34 @@ public class DebugLobbyMenu : Bolt.GlobalEventListener {
 
     private float width = 300f;
 
-    private const int SERVER_MENU_LINE_COUNT = 6;
+    private const int SERVER_MENU_LINE_COUNT = 7;
     private const int CLIENT_MENU_LINE_COUNT = 3;
 
     private const float LINE_HEIGHT = 27.3f;
 
     private int selectedGameMode = 0;
+    private int selectedMap = 0;
+
+    private string[] mapList;
+    private string[] humanReadableMapList;
     void Update() {
         DebugHUD.setValue("IsSever", BoltNetwork.isServer);
         if (BoltNetwork.isClient) {
             DebugHUD.setValue("ping", BoltNetwork.server.PingNetwork);
         }
         DebugHUD.setValue("username", GameManager.instance.CurrentUserName);
+    }
+
+    void Start() {
+        //updateMapList(GameManager.instance.gameMode.GetType().Name);
+        mapList = new string[0];
+        humanReadableMapList = new string[0];
+    }
+
+    private void updateMapList(string gameModeName) {
+        mapList = GameModeManager.GetSupportedMapsForGameMode(gameModeName).ToArray();
+        humanReadableMapList = mapList.Select(s => GameModeManager.GetHumanReadableNameForMap(s)).ToArray();
+        selectedMap = 0;
     }
 
     void OnGUI() {
@@ -32,7 +49,7 @@ public class DebugLobbyMenu : Bolt.GlobalEventListener {
             StartBox(CLIENT_MENU_LINE_COUNT * LINE_HEIGHT);
             DrawClientMenu();
         } else {
-            StartBox((SERVER_MENU_LINE_COUNT + GameModeManager.GameModes.Length) * LINE_HEIGHT);
+            StartBox((SERVER_MENU_LINE_COUNT + GameModeManager.GameModes.Length + mapList.Length) * LINE_HEIGHT);
             DrawServerMenu();
         }
         EndBox();
@@ -58,15 +75,31 @@ public class DebugLobbyMenu : Bolt.GlobalEventListener {
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
-        selectedGameMode = GUILayout.SelectionGrid(selectedGameMode, GameModeManager.GameModeNames, 1);
-        GameManager.instance.gameMode = GameModeManager.GameModes[selectedGameMode];
+        int newGameModeIndex = GUILayout.SelectionGrid(selectedGameMode, GameModeManager.GameModeNames, 1);
+        if (newGameModeIndex != selectedGameMode) {
+            selectedGameMode = newGameModeIndex;
+            IGameMode newGameMode = GameModeManager.GameModes[selectedGameMode];
+            GameManager.instance.gameMode = newGameMode;
+            updateMapList(GameModeManager.GameModeNames[selectedGameMode]);
+        }
         GUILayout.EndHorizontal();
 
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("Map:");
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        selectedMap = GUILayout.SelectionGrid(selectedMap, humanReadableMapList, 1);
+        GUILayout.EndHorizontal();
 
         DrawTeamChangeButtons();
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("Start Game")) {
-            BoltNetwork.LoadScene("ingame");
+            string sceneName = "ingame_" + mapList[selectedMap] + "_" + GameManager.instance.gameMode.GetType().Name;
+            Debug.Log("loading scene \"" + sceneName + "\"");
+            BoltNetwork.LoadScene(sceneName);
         }
         GUILayout.EndHorizontal();
     }
