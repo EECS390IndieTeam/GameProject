5e41c6dcbc2bc470c14aaa5dce88aa7f75e613e5
Christian Gunderman <gundermanc@gmail.com>
2015-11-06 22:40:33 -0500

    Script for using reticle as mouse cursor.

---
+++ Assets/Scripts/Menu System/MenuCursorTexture.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Sets the cursor texture for the game. This cursor texture persists between
+/// scenes until set again.
+/// </summary>
+public class MenuCursorTexture : MonoBehaviour
+{
+    /// <summary>
+    /// The texture to set to the cursor.
+    /// </summary>
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void Start()
+    {
+        // Check for a cursor texture.
+        if (this.cursorTexture == null)
+        {
+            Debug.LogError("MenuCursorTexture script requires a Cursor texture.");
+            return;
+        }
+
+        // Check that mouse hotspot is within the cursor bounds.
+        if (this.hotSpot.x > this.cursorTexture.width || this.hotSpot.y > this.cursorTexture.height)
+        {
+            Debug.LogError("MenuCursorTexture script hotspot is outside of the cursor texture bounds.");
+        }
+
+        Cursor.SetCursor(this.cursorTexture, this.hotSpot, this.cursorMode);
+    }
+}
