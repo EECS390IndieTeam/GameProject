479ab0cc616f6a17f7141e485b058322c45ef117
Steven Miller <yourgamesbeover@gmail.com>
2015-11-11 09:52:56 -0500

    Timer visual improvements

--- Assets/Scripts/HUD/Timer.cs
+++ Assets/Scripts/HUD/Timer.cs
@@ -3,24 +3,23 @@ using UnityEngine.UI;
 using System.Collections;
 
  public class Timer : MonoBehaviour {
+     const int WARNING_TIME = 10;
      public Text timer;
 
      void Update() {
          if (GameManager.instance.CurrentGameState != GameManager.GameState.IN_GAME) {
              timer.text = "";
-             DebugHUD.setValue("TimerState", "Not in game");
          } else if (float.IsPositiveInfinity(GameManager.instance.GameMode.TimeLimit)) {
              timer.text = ((char)236) + ""; //infinity symbol
              this.enabled = false;
-             DebugHUD.setValue("TimerState", "Infinity");
          } else {
              float timeSinceStart = BoltNetwork.serverTime - GameManager.instance.GameStartTime;
              float timeRemaining = GameManager.instance.GameMode.TimeLimit - timeSinceStart;
              int time = (int)timeRemaining;
              int seconds = time % 60;
              int minutes = time / 60;
-             timer.text = minutes + ":" + seconds;
-             DebugHUD.setValue("TimerState", "Active " + minutes + ":" + seconds);
+             bool color = time <= 10;
+             timer.text = (color ? "<color=red>" : "") + (minutes > 0 ? minutes + ":" : "") + (seconds < 10 ? "0" : "") + seconds + (color ? "</color>" : "");
          }
      }
  }
