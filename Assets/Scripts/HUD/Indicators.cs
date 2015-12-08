03b4de72ee9cc0ec436ffbc0530998be0e50d1ea
freshvictory <jsren1@verizon.net>
2015-12-07 21:38:55 -0500

    Added indicators

---
+++ Assets/Scripts/HUD/Indicators.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Indicators : MonoBehaviour {
+
+	private Flag friendlyFlag;
+	private Flag enemyFlag;
+	private FlagCapturePoint friendlyBase;
+	private FlagCapturePoint enemyBase;
+
+	public GameObject friendlyFlagIndicator;
+	public GameObject enemyFlagIndicator;
+	public GameObject friendlyBaseIndicator;
+	public GameObject enemyBaseIndicator;
+
+	private AbstractPlayer player;
+	public Camera camera;
+	public Canvas canvas;
+	private CaptureTheFlagMode ctf;
+	private RectTransform crt;
+
+	// Use this for initialization
+	void Start () {
+		player = (AbstractPlayer)GameManager.instance.CurrentPlayer;
+		ctf = (CaptureTheFlagMode)GameManager.instance.GameMode;
+		crt = canvas.GetComponent<RectTransform>();
+		friendlyBase = ctf.GetCapPointForTeam(player.Team);
+		enemyBase = ctf.GetCapPointForTeam(player.Team==1?2:1);
+		friendlyBaseIndicator.GetComponent<Image>().color = Teams.Colors[player.Team];
+		friendlyFlagIndicator.GetComponent<Image>().color = Teams.Colors[player.Team];
+		enemyBaseIndicator.GetComponent<Image>().color = Teams.Colors[player.Team==1?2:1];
+		enemyFlagIndicator.GetComponent<Image>().color = Teams.Colors[player.Team==1?2:1];
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (!friendlyBase) {
+			friendlyBase = ctf.GetCapPointForTeam(player.Team);
+		} else {
+			if (!friendlyBase.FlagAtBase) {
+				updateIndicator(friendlyBaseIndicator, friendlyBase.transform);
+			}
+		}
+		if (!enemyBase) {
+			enemyBase = ctf.GetCapPointForTeam(player.Team==1?2:1);
+		} else {
+			if (!enemyBase.FlagAtBase) {
+				updateIndicator(enemyBaseIndicator, enemyBase.transform);
+			}
+		}
+		//friendlyFlagIndicator.transform.position = Camera.WorldToScreenPoint(friendlyFlag.transform.position);
+
+	}
+
+	private void updateIndicator(GameObject indicator, Transform source) {
+		RectTransform rt = (RectTransform)indicator.transform;
+		Vector3 point = camera.WorldToViewportPoint(source.position);
+		Vector2 screenposition = new Vector2((point.x*crt.sizeDelta.x)-(crt.sizeDelta.x*0.5f), ((point.y*crt.sizeDelta.y)-(crt.sizeDelta.y*0.5f)));
+		if (point.z > 0f) {
+			indicator.SetActive(true);
+			screenposition.x = Mathf.Sign(screenposition.x) * Mathf.Min (400f, Mathf.Abs(screenposition.x));
+			screenposition.y = Mathf.Sign(screenposition.y) * Mathf.Min (210f, Mathf.Abs(screenposition.y));
+			rt.anchoredPosition = screenposition;
+		} else {
+			indicator.SetActive(false);
+		}
+	}
+}
--- Assets/Scripts/HUD/PlayerGrenades.cs
+++ Assets/Scripts/HUD/PlayerGrenades.cs
@@ -4,67 +4,35 @@ using System.Collections;
 public class PlayerGrenades : MonoBehaviour {
 	public GameObject playerCam;		//Grenade Throw is attached to the Camera
 	public int grenadeCount;
+	private int previous;
+	private int total;
 
 	public ThrowGrenade nadeScript;
-	GameObject nade1;
-	GameObject nade2;
-	GameObject nade3;
-	GameObject nade4;
-	GameObject nade5;
-	// Use this for initialization
-	void Start () {
+	public GameObject[] grenades;
 
-		nade1 = this.transform.Find("Grenade 1").gameObject;
-		nade2 = this.transform.Find("Grenade 2").gameObject;
-		nade3 = this.transform.Find("Grenade 3").gameObject;
-		nade4 = this.transform.Find("Grenade 4").gameObject;
-		nade5 = this.transform.Find("Grenade 5").gameObject;
+	void Start() {
+		total = nadeScript.grenadeAmmo;
+		previous = total;
+		refill();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		grenadeCount = nadeScript.grenadeAmmo;
-		if (grenadeCount < 1) {
-			nade1.SetActive(false);
-			nade2.SetActive(false);
-			nade3.SetActive(false);
-			nade4.SetActive(false);
-			nade5.SetActive(false);
+		if (grenadeCount != previous) {
+			for (int i = grenadeCount; i < grenades.Length; i++) {
+				grenades[i].SetActive(false);
+			}
 		}
-		else if (grenadeCount < 2) {
-			nade1.SetActive(true);
-			nade2.SetActive(false);
-			nade3.SetActive(false);
-			nade4.SetActive(false);
-			nade5.SetActive(false);
+		if (grenadeCount == total) {
+			refill();
 		}
-		else if (grenadeCount < 3) {
-			nade1.SetActive(true);
-			nade2.SetActive(true);
-			nade3.SetActive(false);
-			nade4.SetActive(false);
-			nade5.SetActive(false);
-		}
-		else if (grenadeCount < 4) {
-			nade1.SetActive(true);
-			nade2.SetActive(true);
-			nade3.SetActive(true);
-			nade4.SetActive(false);
-			nade5.SetActive(false);
-		}
-		else if (grenadeCount < 5) {
-			nade1.SetActive(true);
-			nade2.SetActive(true);
-			nade3.SetActive(true);
-			nade4.SetActive(true);
-			nade5.SetActive(false);
-		}
-		else {
-			nade1.SetActive(true);
-			nade2.SetActive(true);
-			nade3.SetActive(true);
-			nade4.SetActive(true);
-			nade5.SetActive(true);
+		previous = grenadeCount;
+	}
+
+	private void refill() {
+		for (int i = 0; i < grenades.Length; i++) {
+			grenades[i].SetActive(true);
 		}
 	}
 }
